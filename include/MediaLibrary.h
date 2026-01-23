e2f26d13bcf0fb78311d3097c7c7d9a621ee39d4
ysfgven <yusuf.guven0@hotmail.com>
2026-01-24 01:40:16 +0300

    Added MediaLibrary and changed existing code made progress in development

---
+++ include/MediaLibrary.h
@@ -0,0 +1,16 @@
+//
+// Created by yusuf on 24.01.2026.
+//
+
+#ifndef MEDIALIBRARY_H
+#define MEDIALIBRARY_H
+
+
+
+class MediaLibrary {
+
+};
+
+
+
+#endif //MEDIALIBRARY_H
---
+++ src/MediaLibrary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yusuf on 24.01.2026.
+//
+
+#include "../include/MediaLibrary.h"
